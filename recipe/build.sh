0b2853b19e487ab92ca65b675feb953b294b1b7f
Daniel Nachun <daniel.nachun@gmail.com>
2026-01-11 23:25:23 -0800

    fix build

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,7 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
+cd checkmate
 go build -buildmode=pie -trimpath -o=${PREFIX}/bin/${PKG_NAME} -ldflags="-s -w"
 go-licenses save . --save_path=license-files \
     --ignore github.com/golang/freetype/raster \
